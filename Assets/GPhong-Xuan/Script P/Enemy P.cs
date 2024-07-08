ce5897d3894e87251245957e24b27b5cd9a3e257
PNGPhong <phongpngps40540@gmail.com>
2024-07-08 15:13:58 +0700

    build map (7)
    bổ sung mở rộng map và có 2 script enemy và player của phong.

---
+++ Assets/GPhong-Xuan/Script P/Enemy P.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyP : MonoBehaviour
+{
+
+    public GameObject coinPrefab; // Kéo Prefab của đồng xu vào đây
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Death();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Death()
+    {
+        // Rơi ra đồng xu
+        Instantiate(coinPrefab, transform.position, Quaternion.identity);
+
+        // Hủy quái
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/GPhong-Xuan/Script P/playerP.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerP : MonoBehaviour
+{
+    public int coinAmount = 0; // Số lượng đồng xu
+    public TMPro.TextMeshProUGUI coinText; // Giao diện hiển thị số lượng đồng xu
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateCoinText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Xử lý khi va chạm với đồng xu
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            coinAmount++;
+            Destroy(other.gameObject); // Hủy đồng xu
+        }  
+    }
+    private void UpdateCoinText()
+    {
+        coinText.text = "Coins: " + coinAmount.ToString();
+    }
+}
