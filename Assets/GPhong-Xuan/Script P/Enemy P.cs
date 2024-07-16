9ab7675275870314dd3dfdce275c55bc7fdd5c3a
PNGPhong <phongpngps40540@gmail.com>
2024-07-16 14:21:05 +0700

    code quai
    setup enemy

--- Assets/GPhong-Xuan/Script P/Enemy P.cs
+++ Assets/GPhong-Xuan/Script P/Enemy P.cs
@@ -3,27 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 public class EnemyP : MonoBehaviour
 {
 
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] public float attackRange = 1.5f;
+    [SerializeField] public float attackCooldown = 2f;
     [SerializeField] private float leftBoundary;
     [SerializeField] private float rightBoundary;
+
+    private Rigidbody2D rb;
+
     //giả sử quái dg di chuyển sang phải
     private bool isRight = true;
 
     //public GameObject coinPrefab; // Kéo Prefab của đồng xu vào đây
 
-    public Transform[] waypoints; // Danh sách các điểm đánh dấu (waypoints) để quái vật tuần tra
+    public Transform player; // Kéo và thả đối tượng người chơi vào trường này
+    public float distanceToPlayer = 2f; // Khoảng cách để xác định quái vật gặp người chơi
 
+    private Animator animator;
+    private float lastAttackTime;
+
+    /*public Transform[] waypoints; // Danh sách các điểm đánh dấu (waypoints) để quái vật tuần tra
     public float viewRange = 10f; // Khoảng cách tầm nhìn
-    public float fovAngle = 45f; // Góc tầm nhìn (độ)
+    public float fovAngle = 45f; // Góc tầm nhìn (độ)*/
 
     // Start is called before the first frame update
     void Start()
     {
         //Death();
+
+        animator = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -32,16 +48,82 @@ public class EnemyP : MonoBehaviour
 
         diChuyenNgang();
         hienTai();
+
+
+        // Tính khoảng cách giữa quái vật và người chơi
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= attackRange)
+        {
+            // Tấn công người chơi
+            Attack();
+            
+        }
+        else
+        {
+            // Di chuyển theo người chơi
+            MoveTowardsPlayer();
+            //currentPosition: vi tri hien tai
+            var currentPosition = transform.localPosition;
+            if (currentPosition.x > rightBoundary)
+            {
+                isRight = false;
+            }
+            else if (currentPosition.x < leftBoundary)
+            {
+                isRight = true;
+            }
+            //scale hiện tai
+            var currentScale = transform.localScale;
+            if (isRight == true && currentScale.x > 0 || isRight == false && currentScale.x < 0)
+            {
+                currentScale.x *= -1;
+            }
+            transform.localScale = currentScale;
+        }
+
+        // Nếu khoảng cách nhỏ hơn hoặc bằng distanceToPlayer, quái vật gặp người chơi
+        if (distance <= distanceToPlayer)
+        {
+            // Xử lý tình huống khi quái vật gặp người chơi ở đây
+            Debug.Log("Quai vat gap nguoi choi!");
+            
+        }
+
+    }
+
+    private void Attack()
+    {
+        
+
+        // Kích hoạt animation tấn công
+        if (Time.time - lastAttackTime >= attackCooldown)
+        {
+            // Thực hiện tấn công (ví dụ: gọi hàm đánh)
+            Debug.Log("Quai vat tan cong nguoi choi!");
+            animator.SetTrigger("Attack1");
+            lastAttackTime = Time.time;
+        }
+        else
+        {
+            // Kích hoạt animation tấn công
+           
+        }
+
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        animator.SetBool("isMoving", true);
+        Vector3 direction = player.position - transform.position;
+        transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
         
     }
 
-
     private void diChuyenNgang()
     {
-        //di chuyển ngang
-        //(1, 0, 0) * 1 * 0.02 = (0.02, 0, 0)
+        animator.SetBool("isMoving", true);
 
-        //var direction = isRight ? Vector3.right : Vector3.left; cách 1
+
         var direction = Vector3.right; //cách 2
         if ((isRight == false))
         {
@@ -72,6 +154,8 @@ public class EnemyP : MonoBehaviour
         }
         transform.localScale = currentScale;
     }
+
+
 
     /*public void Death()
     {
---
+++ Assets/GPhong-Xuan/Script P/EnemyC.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyC : MonoBehaviour
+{
+    public float detectionRangeAttack = 2.5f;  // Phạm vi phát hiện người chơi
+    public float detectionRange = 7f;  // Phạm vi phát hiện người chơi
+    private float stopRange=0.5f;
+    public Transform Player;//follow player
+
+    private float TimeAttackRate = 2f;
+    private float timeAttack;
+
+    private bool right;
+
+    Animator animator;
+    Rigidbody2D rb;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeAttack();
+        followPlayer();
+    }
+    void TimeAttack()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
+        if (distanceToPlayer < detectionRangeAttack)
+        {
+            timeAttack -= Time.deltaTime;
+            if (timeAttack <= 0)
+            {
+                //animation tấn công
+                animator.SetTrigger("Attack1");
+                timeAttack = TimeAttackRate;
+            }
+        }
+    }
+    private void followPlayer()//thấy player thì chạy theo
+    {
+        // Tính khoảng cách giữa quái vật và người chơi
+        float distanceToPlayer = Vector3.Distance(transform.position, Player.position);
+        // Nếu khoảng cách nhỏ hơn phạm vi phát hiện, quái vật sẽ đuổi theo người chơi
+        if (distanceToPlayer <= detectionRange)
+        {
+            if (distanceToPlayer > stopRange)
+            {
+                Vector2 moveDirection = (Player.position - transform.position).normalized;
+                rb.velocity = moveDirection * 1.5f;// Tốc độ di chuyển
+
+                animator.SetBool("isMoving", true);
+
+
+                //xoay mặt
+                if (right && moveDirection.x < 0 || !right && moveDirection.x > 0)
+                {
+                    right = !right;
+                    Vector3 kichThuoc = transform.localScale;
+                    kichThuoc.x = kichThuoc.x * -1;
+                    transform.localScale = kichThuoc;
+
+                }
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
